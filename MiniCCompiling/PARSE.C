ac28c64999e58d43fc21a361778a9cf7b397cd7a
Wu-peihua <13128684834@163.com>
2020-04-16 17:59:41 +0800

    修改编译工具文件

--- MiniCCompiling/PARSE.C
+++ MiniCCompiling/PARSE.C
@@ -9,10 +9,12 @@
 #include "UTIL.H"
 #include "SCAN.H"
 #include "PARSE.H"
+#include<string>
+using namespace std;
 
-static TokenType token; /* holds current token */
+static TypeToken token; /* holds current token */
 
-/* function prototypes for recursive calls */
+                        /* function prototypes for recursive calls */
 static TreeNode * stmt_sequence(void);
 static TreeNode * statement(void);
 static TreeNode * if_stmt(void);
@@ -20,255 +22,317 @@ static TreeNode * repeat_stmt(void);
 static TreeNode * assign_stmt(void);
 static TreeNode * read_stmt(void);
 static TreeNode * write_stmt(void);
-static TreeNode * while_stmt(void);
-static TreeNode * dowhile_stmt(void);
-static TreeNode * for_stmt(void);
 static TreeNode * exp(void);
 static TreeNode * simple_exp(void);
 static TreeNode * term(void);
 static TreeNode * factor(void);
 
-static void syntaxError(char * message)
-{ fprintf(listing,"\n>>> ");
-  fprintf(listing,"Syntax error at line %d: %s",lineno,message);
-  Error = TRUE;
+/*TINY的文法扩充*/
+static TreeNode * while_stmt(void);
+static TreeNode * dowhile_stmt(void);
+static TreeNode * for_stmt(void);
+
+
+//static void syntaxError(char * message)
+static char* syntaxError(char * message)
+{
+    string result;
+    //fprintf(listing, "\n>>> ");
+    //fprintf(listing, "Syntax error at line %d: %s", lineno, message);
+    string temp = message;
+    result += "\r\n>>>Syntax error at line " + lineno + temp;
+    Error = TRUE;
+
+    char* r = new char[result.size() + 1];
+    strcpy_s(r, result.size() + 1, result.c_str());
+    return r;
 }
 
-static void match(TokenType expected)
-{ if (token == expected) token = getToken();
-  else {
-    syntaxError("unexpected token -> ");
-    printToken(token,tokenString);
-    fprintf(listing,"      ");
-  }
+static void match(TypeToken expected)
+{
+    if (token == expected) token = getToken();
+    else {
+        syntaxError("unexpected token -> ");
+        printToken(token, tokenString);
+        fprintf(listing, "      ");
+    }
 }
 
 TreeNode * stmt_sequence(void)
-{ TreeNode * t = statement();
-  TreeNode * p = t;
-  while ((token!=ENDFILE) && (token!=END) &&
-         (token!=ELSE) && (token!=UNTIL) && 
-         (token!=ENDDO) && (token!=ENDWHILE))
-  { TreeNode * q;
-    if(token==SEMI){
-      match(SEMI);
-      if(token==ENDDO || token==ENDWHILE) break;
-      q = statement();
-      if (q!=NULL) {
-        if (t==NULL) t = p = q;
-        else /* now p cannot be NULL either */
-        { p->sibling = q;
-          p = q;
-        }
-      }
-    }else
+{
+    TreeNode * t = statement();
+    TreeNode * p = t;
+    while ((token != ENDFILE) && (token != END) &&
+        (token != ELSE) && (token != UNTIL) && (token != WHILE)&& (token != ENDDO) && (token != ENDWHILE))
     {
-      match(token);
+        TreeNode * q;
+        if (token == SEMI) {
+            match(SEMI);
+        }
+        q = statement();
+        if (q != NULL) {
+            if (t == NULL) t = p = q;
+            else /* now p cannot be NULL either */
+            {
+                p->sibling = q;
+                p = q;
+            }
+        }
     }
-    
-  }
-  return t;
+    return t;
 }
 
 
-//P394 
+//P394
 //lineno: 961
 TreeNode * statement(void)
-{ TreeNode * t = NULL;
-  switch (token) {
-    case IF : t = if_stmt(); break;
-    case REPEAT : t = repeat_stmt(); break;
-    case ID : t = assign_stmt(); break;
-    case READ : t = read_stmt(); break;
-    case WRITE : t = write_stmt(); break;
-    case WHILE : t = while_stmt(); break;
-    case DO : t = dowhile_stmt(); break;
-    case FOR : t = for_stmt(); break;
-    default : syntaxError("unexpected token -> ");
-              printToken(token,tokenString);
-              token = getToken();
-              break;
-  } /* end case */
-  return t;
+{
+    TreeNode * t = NULL;
+    switch (token) {
+    case IF: t = if_stmt(); break;
+    case REPEAT: t = repeat_stmt(); break;
+    case ID: t = assign_stmt(); break;
+    case READ: t = read_stmt(); break;
+    case WRITE: t = write_stmt(); break;
+    case WHILE: t = while_stmt(); break;
+    case DO: t = dowhile_stmt(); break;
+    case FOR: t = for_stmt(); break;
+    case ENDDO: break;
+    case ENDWHILE: break;
+    default: syntaxError("unexpected token -> ");
+        printToken(token, tokenString);
+        token = getToken();
+        break;
+    } /* end case */
+    return t;
 }
 
+//
+////P394
+////lineno: 977
+//TreeNode * if_stmt(void)
+//{
+//	TreeNode * t = newStmtNode(IfK);
+//	match(IF);
+//	if (t != NULL) t->child[0] = exp();
+//	match(THEN);
+//	if (t != NULL) t->child[1] = stmt_sequence();
+//	if (token == ELSE) {
+//		match(ELSE);
+//		if (t != NULL) t->child[2] = stmt_sequence();
+//	}
+//	match(END);
+//	return t;
+//}
 
-//P394 
-//lineno: 977
+//更改后的if
 TreeNode * if_stmt(void)
-{ TreeNode * t = newStmtNode(IfK);
-  match(IF);
-  if (t!=NULL) t->child[0] = exp();
-  //match(THEN);
-  if (t!=NULL) t->child[1] = stmt_sequence();
-  if (token==ELSE) {
-    match(ELSE);
-    if (t!=NULL) t->child[2] = stmt_sequence();
-  }
-  //match(END);
-  return t;
+{
+    TreeNode * t = newStmtNode(IfK);
+    match(IF);
+    if (t != NULL) t->child[0] = exp();
+    if (t != NULL) t->child[1] = stmt_sequence();
+    if (token == ELSE) {
+        match(ELSE);
+        if (t != NULL) t->child[2] = stmt_sequence();
+    }
+    return t;
 }
 
-//P394 
+
+//P394
 //lineno:991
 TreeNode * repeat_stmt(void)
-{ TreeNode * t = newStmtNode(RepeatK);
-  match(REPEAT);
-  if (t!=NULL) t->child[0] = stmt_sequence();
-  match(UNTIL);
-  if (t!=NULL) t->child[1] = exp();
-  return t;
+{
+    TreeNode * t = newStmtNode(RepeatK);
+    match(REPEAT);
+    if (t != NULL) t->child[0] = stmt_sequence();
+    match(UNTIL);
+    if (t != NULL) t->child[1] = exp();
+    return t;
 }
 
 TreeNode * assign_stmt(void)
-{ TreeNode * t = newStmtNode(AssignK);
-  if ((t!=NULL) && (token==ID))
-    t->attr.name = copyString(tokenString);
-  match(ID);
-  match(ASSIGN);
-  if (t!=NULL) t->child[0] = exp();
-  return t;
+{
+    TreeNode * t = newStmtNode(AssignK);
+    if ((t != NULL) && (token == ID))
+        t->attr.name = copyString(tokenString);
+    match(ID);
+    if (token == ASSIGN) {
+        match(ASSIGN);
+    }
+    else if( token == ADDITIVE){  //加法赋值号的文法扩充
+        match(ADDITIVE);
+        t->kind.stmt = AdditiveK;
+    }
+    if (t != NULL) t->child[0] = exp();
+    return t;
 }
 
 TreeNode * read_stmt(void)
-{ TreeNode * t = newStmtNode(ReadK);
-  match(READ);
-  if ((t!=NULL) && (token==ID))
-    t->attr.name = copyString(tokenString);
-  match(ID);
-  return t;
+{
+    TreeNode * t = newStmtNode(ReadK);
+    match(READ);
+    if ((t != NULL) && (token == ID))
+        t->attr.name = copyString(tokenString);
+    match(ID);
+    return t;
 }
 
 TreeNode * write_stmt(void)
-{ TreeNode * t = newStmtNode(WriteK);
-  match(WRITE);
-  if (t!=NULL) t->child[0] = exp();
-  return t;
-}
-
-TreeNode * while_stmt(void){
-  TreeNode * t = newStmtNode(WhileK);
-  match(WHILE);
-  // match(LPAREN);
-  if (t!=NULL) t->child[0] = exp();
-  // match(RPAREN);
-  // if (t!=NULL) t->child[1] = stmt_sequence();
-  //match(ENDWHILE);
-  return t;
-}
-
-TreeNode * dowhile_stmt(void){
-  TreeNode * t = newStmtNode(DoK);
-  match(DO);
-  if(t != NULL) t->child[0] = stmt_sequence();
-  return t;
-}
-
-TreeNode * for_stmt(void){
-  TreeNode * t = newStmtNode(ForK);
-  match(FOR);
-  if((t!=NULL) && (token == ID))
-    t->attr.name = copyString(tokenString);
-  match(ID);
-  match(ASSIGN);
-  if(t!=NULL) t->child[0] = simple_exp();
-  if((t!=NULL)&&((token==TO)||(token==DOWNTO))){
-    match(token);
-    t->child[1] = simple_exp();
-  }
-  match(DO);
-  if(t!=NULL) t->child[2] = stmt_sequence();
-  match(ENDDO);
-
-  return t;
+{
+    TreeNode * t = newStmtNode(WriteK);
+    match(WRITE);
+    if (t != NULL) t->child[0] = exp();
+    return t;
 }
 
 TreeNode * exp(void)
-{ TreeNode * t = simple_exp();
-  if ((token==LT)||(token==EQ)) {
-    TreeNode * p = newExpNode(OpK);
-    if (p!=NULL) {
-      p->child[0] = t;
-      p->attr.op = token;
-      t = p;
+{
+    TreeNode * t = simple_exp();
+    if ((token == LT) || (token == EQ)) {
+        TreeNode * p = newExpNode(OpK);
+        if (p != NULL) {
+            p->child[0] = t;
+            p->attr.op = token;
+            t = p;
+        }
+        match(token);
+        if (t != NULL)
+            t->child[1] = simple_exp();
     }
-    match(token);
-    if (t!=NULL)
-      t->child[1] = simple_exp();
-  }
-  return t;
+    return t;
 }
 
 TreeNode * simple_exp(void)
-{ TreeNode * t = term();
-  while ((token==PLUS)||(token==MINUS))
-  { TreeNode * p = newExpNode(OpK);
-    if (p!=NULL) {
-      p->child[0] = t;
-      p->attr.op = token;
-      t = p;
-      match(token);
-      t->child[1] = term();
+{
+    TreeNode * t = term();
+    while ((token == PLUS) || (token == MINUS))
+    {
+        TreeNode * p = newExpNode(OpK);
+        if (p != NULL) {
+            p->child[0] = t;
+            p->attr.op = token;
+            t = p;
+            match(token);
+            t->child[1] = term();
+        }
     }
-  }
-  return t;
+    return t;
 }
 
 TreeNode * term(void)
-{ TreeNode * t = factor();
-  while ((token==TIMES)||(token==OVER))
-  { TreeNode * p = newExpNode(OpK);
-    if (p!=NULL) {
-      p->child[0] = t;
-      p->attr.op = token;
-      t = p;
-      match(token);
-      p->child[1] = factor();
+{
+    TreeNode * t = factor();
+    while ((token == TIMES) || (token == OVER) || (token == POWER))
+    {
+        TreeNode * p = newExpNode(OpK);
+        if (p != NULL) {
+            p->child[0] = t;
+            p->attr.op = token;
+            t = p;
+            match(token);
+            p->child[1] = factor();
+        }
     }
-  }
-  return t;
+    return t;
 }
 
 TreeNode * factor(void)
-{ TreeNode * t = NULL;
-  switch (token) {
-    case NUM :
-      t = newExpNode(ConstK);
-      if ((t!=NULL) && (token==NUM))
-        t->attr.val = atoi(tokenString);
-      match(NUM);
-      break;
-    case ID :
-      t = newExpNode(IdK);
-      if ((t!=NULL) && (token==ID))
-        t->attr.name = copyString(tokenString);
-      match(ID);
-      break;
-    case LPAREN :
-      match(LPAREN);
-      t = exp();
-      match(RPAREN);
-      break;
+{
+    TreeNode * t = NULL;
+    switch (token) {
+    case NUM:
+        t = newExpNode(ConstK);
+        if ((t != NULL) && (token == NUM))
+            t->attr.val = atoi(tokenString);
+        match(NUM);
+        break;
+    case ID:
+        t = newExpNode(IdK);
+        if ((t != NULL) && (token == ID))
+            t->attr.name = copyString(tokenString);
+        match(ID);
+        break;
+    case LPAREN:
+        match(LPAREN);
+        t = exp();
+        match(RPAREN);
+        break;
     default:
-      syntaxError("unexpected token -> ");
-      printToken(token,tokenString);
-      token = getToken();
-      break;
+        syntaxError("unexpected token -> ");
+        printToken(token, tokenString);
+        token = getToken();
+        break;
     }
-  return t;
+    return t;
 }
+
+
+/*TINY的文法扩充*/
+static TreeNode * while_stmt(void) {
+    TreeNode * t = newStmtNode(WhileK);
+    match(WHILE);
+    if (t != NULL) {
+        t->child[0] = exp();
+    }
+    match(DO);
+    if (t != NULL) {
+        t->child[1] = stmt_sequence();
+    }
+    match(ENDWHILE);
+    return t;
+}
+
+static TreeNode * dowhile_stmt(void) {
+    TreeNode * t = newStmtNode(DowhileK);
+    match(DO);
+    if (t != NULL) {
+        t->child[0] = stmt_sequence();
+    }
+    match(WHILE);
+    if (t != NULL) {
+        t->child[1] = exp();
+    }
+    return t;
+}
+
+
+static TreeNode * for_stmt(void) {
+    TreeNode * t = newStmtNode(ForIncK);
+    match(FOR);
+    if (t != NULL) {
+        t->child[0] = assign_stmt();
+        if (token == TO) {
+            match(TO);
+        }
+        else if (token == DOWNTO) {
+            match(DOWNTO);
+            t->kind.stmt = ForDecK;
+        }
+        t->child[1] = simple_exp();
+        match(DO);
+        t->child[2] = stmt_sequence();
+        match(ENDDO);
+    }
+    return t;
+}
+
+
 
 /****************************************/
 /* the primary function of the parser   */
 /****************************************/
-/* Function parse returns the newly 
- * constructed syntax tree
- */
+/* Function parse returns the newly
+* constructed syntax tree
+*/
 TreeNode * parse(void)
-{ TreeNode * t;
-  token = getToken();
-  t = stmt_sequence();
-  if (token!=ENDFILE)
-    syntaxError("Code ends before file\n");
-  return t;
+{
+    TreeNode * t;
+    token = getToken();
+    t = stmt_sequence();
+    if (token != ENDFILE)
+        syntaxError("Code ends before file\n");
+    return t;
 }
+
--- MiniCCompiling/UTIL.C
+++ MiniCCompiling/UTIL.C
@@ -8,114 +8,198 @@
 
 #include "GLOBALS.H"
 #include "UTIL.H"
-
-/* Procedure printToken prints a token 
+#include<string>
+#include<sstream>
+using namespace std;
+/* Procedure printToken prints a token
  * and its lexeme to the listing file
  */
-void printToken( TokenType token, const char* tokenString )
-{ switch (token)
-  { case IF:
-    case THEN:
-    case ELSE:
-    case END:
-    case REPEAT:
-    case UNTIL:
-    case READ:
-    case WRITE:
-    case WHILE:
-    case DO:
-    case FOR:
-    case ENDWHILE:
-    case ENDDO:
-    case TO:
-    case DOWNTO:
-      fprintf(listing,
-         "reserved word: %s\n",tokenString);
-      break;
-    case ASSIGN: fprintf(listing,":=\n"); break;
-    case LT: fprintf(listing,"<\n"); break;
-    case EQ: fprintf(listing,"=\n"); break;
-    case LPAREN: fprintf(listing,"(\n"); break;
-    case RPAREN: fprintf(listing,")\n"); break;
-    case SEMI: fprintf(listing,";\n"); break;
-    case PLUS: fprintf(listing,"+\n"); break;
-    case MINUS: fprintf(listing,"-\n"); break;
-    case TIMES: fprintf(listing,"*\n"); break;
-    case OVER: fprintf(listing,"/\n"); break;
-    case ENDFILE: fprintf(listing,"EOF\n"); break;
-    case NUM:
-      fprintf(listing,
-          "NUM, val= %s\n",tokenString);
-      break;
-    case ID:
-      fprintf(listing,
-          "ID, name= %s\n",tokenString);
-      break;
-    case ERROR:
-      fprintf(listing,
-          "ERROR: %s\n",tokenString);
-      break;
-    default: /* should never happen */
-      fprintf(listing,"Unknown token: %d\n",token);
-  }
+char* printToken(TypeToken token, const char* tokenString)
+{
+   string result;
+   string temp = tokenString;
+   switch (token)
+   {
+   case IF:
+   case THEN:
+   case ELSE:
+   case END:
+   case REPEAT:
+   case UNTIL:
+   case READ:
+   case WRITE:
+   case DO:
+   case WHILE:
+   case ENDWHILE:
+   case FOR:
+   case TO:
+   case DOWNTO:
+   case ENDDO:
+
+       //fprintf(listing,"reserved word: %s\n", tokenString);
+       /*strcat_s(result,15, "reserved word: ");
+       strcat_s(result,sizeof(tokenString), tokenString);
+       strcat_s(result,4, "\r\n");*/
+       result += "reserved word: " + temp + "\r\n";
+       break;
+   case ASSIGN:
+       //fprintf(listing, ":=\n");
+       //strcat_s(result, 6,":=\r\n");
+       result += ":=\r\n";
+       break;
+   case LT:
+       //fprintf(listing, "<\n");
+       //strcat_s(result,5, "<\r\n");
+       result += "<\r\n";
+       break;
+   case EQ:
+       //fprintf(listing, "=\n");
+       //strcat_s(result,5, "=\r\n");
+       result += "=\r\n";
+       break;
+   case LPAREN:
+       //fprintf(listing, "(\n");
+       //strcat_s(result,5, "(\r\n");
+       result += "(\r\n";
+       break;
+   case RPAREN:
+       //fprintf(listing, ")\n");
+       //strcat_s(result, 5,")\r\n");
+       result += ")\r\n";
+       break;
+   case SEMI:
+       //fprintf(listing, ";\n");
+       //strcat_s(result,5, ";\r\n");
+       result += ";\r\n";
+       break;
+   case PLUS:
+       //fprintf(listing, "+\n");
+       //strcat_s(result,5, "+\r\n");
+       result += "+\r\n";
+       break;
+   case MINUS:
+       //fprintf(listing, "-\n");
+       //strcat_s(result,5, "-\r\n");
+       result += "-\r\n";
+       break;
+   case TIMES:
+       //fprintf(listing, "*\n");
+       //strcat_s(result,5, "*\r\n");
+       result += "*\r\n";
+       break;
+   case POWER:
+       result += "^\r\n";
+       break;
+   case OVER:
+       //fprintf(listing, "/\n");
+       //strcat_s(result,5, "/\r\n");
+       result += "/\r\n";
+       break;
+   case ENDFILE:
+       //fprintf(listing, "EOF\n");
+       //strcat_s(result, 7,"EOF\r\n");
+       result += "EOF\r\n";
+       break;
+   case NUM:
+       //fprintf(listing,"NUM, val= %s\n", tokenString);
+       /*strcat_s(result, 14,"NUM, val= \r\n");
+       strcat_s(result, sizeof(tokenString), tokenString);
+       strcat_s(result, 4,"\r\n");*/
+       result += "NUM, val= " + temp + "\r\n";
+       break;
+   case ID:
+       //fprintf(listing,"ID, name= %s\n", tokenString);
+       /*strcat_s(result, sizeof("ID, name= \r\n"), "ID, name= \r\n");
+       strcat_s(result,sizeof(tokenString), tokenString);
+       strcat_s(result,4, "\r\n");*/
+       result += "ID, name= " + temp + "\r\n";
+       break;
+   case ERRO:
+       //fprintf(listing,"ERROR: %s\n", tokenString);
+       /*strcat_s(result,7, "ERROR: ");
+       strcat_s(result,sizeof(tokenString), tokenString);
+       strcat_s(result, 4,"\r\n");*/
+       result += "ERROR: " + temp + "\r\n";
+       break;
+   case ADDITIVE:
+       result += "+=\r\n";
+       break;
+   default: /* should never happen */
+       //fprintf(listing, "Unknown token: %d\n", token);
+       //strcat_s(result, sizeof("Unknown token: "), "Unknown token: ");
+       result += "Unknown token: \r\n";
+       //strcat(result, token);
+       //strcat_s(result,4, "\r\n");
+   }
+   char*r = new char[result.size() + 1];
+   strcpy_s(r, result.size() + 1, result.c_str());
+   return r;
 }
 
 /* Function newStmtNode creates a new statement
- * node for syntax tree construction
- */
+* node for syntax tree construction
+*/
 TreeNode * newStmtNode(StmtKind kind)
-{ TreeNode * t = (TreeNode *) malloc(sizeof(TreeNode));
-  int i;
-  if (t==NULL)
-    fprintf(listing,"Out of memory error at line %d\n",lineno);
-  else {
-    for (i=0;i<MAXCHILDREN;i++) t->child[i] = NULL;
-    t->sibling = NULL;
-    t->nodekind = StmtK;
-    t->kind.stmt = kind;
-    t->lineno = lineno;
-  }
-  return t;
+{
+   TreeNode * t = (TreeNode *)malloc(sizeof(TreeNode));
+   int i;
+   if (t == NULL)
+       fprintf(listing, "Out of memory error at line %d\n", lineno);
+   else {
+       for (i = 0; i<MAXCHILDREN; i++) t->child[i] = NULL;
+       t->sibling = NULL;
+       t->nodekind = StmtK;
+       t->kind.stmt = kind;
+       t->lineno = lineno;
+   }
+   return t;
 }
 
-/* Function newExpNode creates a new expression 
- * node for syntax tree construction
- */
+/* Function newExpNode creates a new expression
+* node for syntax tree construction
+*/
 TreeNode * newExpNode(ExpKind kind)
-{ TreeNode * t = (TreeNode *) malloc(sizeof(TreeNode));
-  int i;
-  if (t==NULL)
-    fprintf(listing,"Out of memory error at line %d\n",lineno);
-  else {
-    for (i=0;i<MAXCHILDREN;i++) t->child[i] = NULL;
-    t->sibling = NULL;
-    t->nodekind = ExpK;
-    t->kind.exp = kind;
-    t->lineno = lineno;
-    t->type = Void;
-  }
-  return t;
+{
+   TreeNode * t = (TreeNode *)malloc(sizeof(TreeNode));
+   int i;
+   if (t == NULL)
+       fprintf(listing, "Out of memory error at line %d\n", lineno);
+   else {
+       for (i = 0; i<MAXCHILDREN; i++) t->child[i] = NULL;
+       t->sibling = NULL;
+       t->nodekind = ExpK;
+       t->kind.exp = kind;
+       t->lineno = lineno;
+       t->type = Void;
+   }
+   return t;
 }
 
 /* Function copyString allocates and makes a new
- * copy of an existing string
- */
-char * copyString(char * s)
-{ int n;
-  char * t;
-  if (s==NULL) return NULL;
-  n = strlen(s)+1;
-  t = new char[n];
-  //t = malloc(n);
-  if (t==NULL)
-    fprintf(listing,"Out of memory error at line %d\n",lineno);
-  else strcpy(t,s);
-  return t;
+* copy of an existing string
+*/
+char* copyString(char * s)
+{
+   int n;
+   char * t ;
+   if (s == NULL) return NULL;
+   n = strlen(s) + 1;
+   t = new char[n];
+   //t = (char*)malloc(n);
+   //if (t == NULL) {
+   //	//fprintf(listing, "Out of memory error at line %d\n", lineno);
+   // 	MessageBox(NULL, TEXT("Out of memory error at line "), TEXT("骗人了吧!"), MB_OK);
+   //}
+   //else
+   //	strcpy_s(t, sizeof(s), s);
+   strcpy_s(t, n, s);
+
+   return t;
 }
 
 /* Variable indentno is used by printTree to
- * store current number of spaces to indent
- */
+* store current number of spaces to indent
+*/
 static int indentno = 0;
 
 /* macros to increase/decrease indentation */
@@ -123,75 +207,149 @@ static int indentno = 0;
 #define UNINDENT indentno-=2
 
 /* printSpaces indents by printing spaces */
-static void printSpaces(void)
-{ int i;
-  for (i=0;i<indentno;i++)
-    fprintf(listing," ");
+static char* printSpaces(void)
+{
+   string result ;
+   int i;
+   for (i = 0; i < indentno; i++) {
+       //fprintf(listing, " ");
+       result += " ";
+   }
+
+   char* r = new char[result.size()+1];
+   strcpy_s(r, result.size() + 1, result.c_str());
+   return r;
 }
 
-/* procedure printTree prints a syntax tree to the 
- * listing file using indentation to indicate subtrees
- */
-void printTree( TreeNode * tree )
-{ int i;
-  INDENT;
-  while (tree != NULL) {
-    printSpaces();
-    if (tree->nodekind==StmtK)
-    { switch (tree->kind.stmt) {
-        case IfK:
-          fprintf(listing,"If\n");
-          break;
-        case RepeatK:
-          fprintf(listing,"Repeat\n");
-          break;
-        case AssignK:
-          fprintf(listing,"Assign to: %s\n",tree->attr.name);
-          break;
-        case ReadK:
-          fprintf(listing,"Read: %s\n",tree->attr.name);
-          break;
-        case WriteK:
-          fprintf(listing,"Write\n");
-          break;
-        case WhileK:
-          fprintf(listing,"While\n");
-          break;
-        case DoK:
-          fprintf(listing,"Do\n");
-          break;
-        case ForK:
-          fprintf(listing,"For\n");
-          break;
-        case ForDecK:
-          fprintf(listing,"Fordec\n");
-          break;
-        default :
-          fprintf(listing,"Unknown ExpNode kind\n");
-          break;
-      }
-    }
-    else if (tree->nodekind==ExpK)
-    { switch (tree->kind.exp) {
-        case OpK:
-          fprintf(listing,"Op: ");
-          printToken(tree->attr.op,"\0");
-          break;
-        case ConstK:
-          fprintf(listing,"Const: %d\n",tree->attr.val);
-          break;
-        case IdK:
-          fprintf(listing,"Id: %s\n",tree->attr.name);
-          break;
-        default:
-          fprintf(listing,"Unknown ExpNode kind\n");
-          break;
-      }
-    }
-    else fprintf(listing,"Unknown node kind\n");
-    for (i=0;i<MAXCHILDREN;i++)
-         printTree(tree->child[i]);
-    tree = tree->sibling;
-  }
-  UNINDENT;
+/* procedure printTree prints a syntax tree to the
+* listing file using indentation to indicate subtrees
+*/
+char* printTree(TreeNode * tree)
+{
+   string result = "";
+   char* temp;
+   int i;
+   INDENT;
+   while (tree != NULL) {
+       temp = printSpaces();
+       string name;
+       string val;
+       //strcat_s(result, sizeof(temp), temp);
+       result += temp;
+       if (tree->nodekind == StmtK)
+       {
+           switch (tree->kind.stmt) {
+           case IfK:
+               //fprintf(listing, "If\n");
+               //strcat_s(result, 6,"If\r\n");
+               result += "If\r\n";
+               break;
+           case RepeatK:
+               //fprintf(listing, "Repeat\n");
+               //strcat_s(result,10, "Repeat\r\n");
+               result += "Repeat\r\n";
+               break;
+           case AssignK:
+               //fprintf(listing, "Assign to: %s\n", tree->attr.name);
+               /*strcat_s(result,sizeof("Assign to: "), "Assign to: ");
+               strcat_s(result,sizeof(tree->attr.name), tree->attr.name);
+               strcat_s(result,4, "\r\n");*/
+               name = tree->attr.name;
+               result += "Assign to: " + name + "\r\n";
+               break;
+           case ReadK:
+               //fprintf(listing, "Read: %s\n", tree->attr.name);
+               /*strcat_s(result,6, "Read: ");
+               strcat_s(result, sizeof(tree->attr.name),tree->attr.name);
+               strcat_s(result,4, "\r\n");*/
+               name = tree->attr.name;
+               result += "Read: " + name + "\r\n";
+               break;
+           case WriteK:
+               //fprintf(listing, "Write\n");
+               //strcat_s(result,sizeof("Write\r\n"), "Write\r\n");
+               result += "Write\r\n";
+               break;
+           case ForDecK:
+               result += "for\r\n"  ;
+               break;
+           case ForIncK:
+               result += "for\r\n";
+               break;
+           case DowhileK:
+               result += "do\r\n";
+               break;
+           case WhileK:
+               result += "while\r\n";
+               break;
+           case AdditiveK:
+               name = tree->attr.name;
+               result += "Add and assign to: " + name + "\r\n";
+           default:
+               //fprintf(listing, "Unknown ExpNode kind\n");
+               //strcat_s(result, sizeof("Unknown ExpNode kind\r\n"),"Unknown ExpNode kind\r\n");
+               result += "Unknown ExpNode kind\r\n";
+               break;
+           }
+       }
+       else if (tree->nodekind == ExpK)
+       {
+
+           stringstream ss;
+           ss << tree->attr.val;
+           string temp;
+           string val;
+           string name;
+
+
+           switch (tree->kind.exp) {
+           case OpK:
+               //fprintf(listing, "Op: ");
+               //strcat_s(result,4, "Op: ");
+               result += "Op: ";
+               temp = printToken(tree->attr.op, "\0");
+               //strcat_s(result, sizeof(temp),temp);
+               result += temp;
+               break;
+           case ConstK:
+               //fprintf(listing, "Const: %d\n", tree->attr.val);
+               /*strcat_s(result,sizeof("Const: "), "Const: ");
+               strcat_s(result, sizeof(ss.str().c_str()),ss.str().c_str());
+               strcat_s(result, 4,"\r\n");*/
+               val = to_string(tree->attr.val);
+               result += "Const: " + val + "\r\n";
+               break;
+           case IdK:
+               //fprintf(listing, "Id: %s\n", tree->attr.name);
+               /*strcat_s(result,4, "Id: ");
+               strcat_s(result, sizeof(tree->attr.name),tree->attr.name);
+               strcat_s(result,4, "\r\n");*/
+               name = tree->attr.name;
+               result += "Id: " + name + "\r\n";
+               break;
+           default:
+               //fprintf(listing, "Unknown ExpNode kind\n");
+               //strcat_s(result,sizeof("Unknown ExpNode kind\r\n"), "Unknown ExpNode kind\r\n");
+               result += "Unknown ExpNode kind\r\n";
+               break;
+           }
+       }
+       else {
+           //fprintf(listing, "Unknown node kind\n");
+           //strcat_s(result, sizeof("Unknown node kindr\n"),"Unknown node kindr\n");
+           result += "Unknown node kindr\r\n";
+       }
+       for (i = 0; i < MAXCHILDREN; i++) {
+           string temp;
+           temp = printTree(tree->child[i]);
+           //strcat_s(result, sizeof(temp),temp);
+           result += temp;
+       }
+       tree = tree->sibling;
+   }
+   UNINDENT;
+   char * r = new char[result.size() + 1];
+   strcpy_s(r, result.size() + 1, result.c_str());
+   return r;
 }
+
